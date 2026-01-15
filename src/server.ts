a0cd353ea5b1792ebd70e007af9c2ca6c377cfa7
Kiara Hoheb <121005877+kxtara@users.noreply.github.com>
2026-01-15 18:04:15 -0400

    Add path for API docs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import userRoutes from "./routes/user.route.js"
+import { swaggerUi, swaggerSpec } from "./swagger.js";
+import userRoutes from "./routes/user.route.js";
 import type { Request, Response } from "express";
 import { errorHandler } from "./middleware/error.middleware.js";
 
@@ -11,7 +12,9 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Server is running just fine!");
 });
 
-app.use("/api/users",userRoutes)
+app.use("/api/users", userRoutes);
+// Serve Swagger UI at /api-docs
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorHandler);
 export default app;
